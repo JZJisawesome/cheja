c23750f0e4abbfd8c387c45e6999ce9cae58565f
John Jekel <JZJisawesome123@hotmail.com>
2019-04-18 21:09:29 -0400

    work on ui

--- src/io/github/jzjisawesome/cheja/CLI.java
+++ src/io/github/jzjisawesome/cheja/CLI.java
@@ -22,6 +22,7 @@
 */
 package io.github.jzjisawesome.cheja;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class CLI//will eventually take over from main function with actual user interaction
@@ -32,33 +33,43 @@ public class CLI//will eventually take over from main function with actual user 
     }
     
     private Board board;
-    private boolean byeByeSeeYouLater = false;
     
     private static enum Command
     {
-        invalid, print, move, list, exit,
+        invalid, print, move, list, save, load, help, about, exit, 
     };
     
     public void begin()
     {
         Scanner input = new Scanner(System.in);
         String enteredLine;
-        String commandString;
+        String commandString = "";
         Command command;
+        
+        boolean byeByeSeeYouLater = false;
         
         while (!byeByeSeeYouLater)
         {
             System.out.print("cheja▹");//command prompt
             
-            enteredLine = input.nextLine();
-            input = new Scanner(enteredLine);
-            
-            commandString = input.next();
+            try
+            {
+                enteredLine = input.nextLine();
+                input = new Scanner(enteredLine);
+
+                commandString = input.next();
+            }
+            catch (NoSuchElementException e)//happens if user presses enter key without entering anything
+            {
+                input = new Scanner(System.in);//so we can read new user input again
+                continue;//restart prompt
+            }
             
             try
             {
                 command = Command.valueOf(commandString);
             }
+
             catch (IllegalArgumentException e)
             {
                 command = Command.invalid;
@@ -71,11 +82,6 @@ public class CLI//will eventually take over from main function with actual user 
                     this.printBrd();
                     break;
                 }
-                case exit:
-                {
-                    byeByeSeeYouLater = true;
-                    break;
-                }
                 case move:
                 {
                     this.move(input);
@@ -86,11 +92,44 @@ public class CLI//will eventually take over from main function with actual user 
                     this.list(input);
                     break;
                 }
+                case save:
+                {
+                    String filename = input.nextLine();
+                    
+                    System.out.println("Saving board to: \"" + filename + "\"...");
+                    this.board.save(filename);
+                    System.out.println("Save complete!");
+                    break;
+                }
+                case load:
+                {
+                    String filename = input.nextLine();
+                    
+                    System.out.println("Loading board from: \"" + filename + "\"...");
+                    this.board.load(filename);
+                    System.out.println("Load complete!");
+                    break;
+                }
+                case exit:
+                {
+                    byeByeSeeYouLater = true;
+                    break;
+                }
+                case help:
+                {
+                    saveTheUserFromHeadaches();
+                    break;
+                }
+                case about:
+                {
+                    displayAbout();
+                    break;
+                }
                 case invalid:
                 default:
                 {
                     System.out.println("Invalid command: \"" + commandString + "\"");
-                    System.out.println("Type \"help\" for help");//ha ha no help function yet
+                    System.out.println("Type \"help\" for help");
                     break;
                 }
             }
@@ -317,20 +356,28 @@ public class CLI//will eventually take over from main function with actual user 
         {
             from = input.next();
             
-            byte fromY = (byte) (7 - (Character.getNumericValue(from.charAt(1)) - 1));//value of 0 to 7 from top
-            byte fromX = (byte) (from.charAt(0) - 'a');//value of 0 to 7
-            
-            System.out.print("Valid moves from \"" + from + "\" to: ");
-            
-            for (byte i = 0; i < 8; ++i)//loop for rows
+            try
             {
-                for (byte j = 0; j < 8; ++j)//loop for coloums
+                byte fromY = (byte) (7 - (Character.getNumericValue(from.charAt(1)) - 1));//value of 0 to 7 from top
+                byte fromX = (byte) (from.charAt(0) - 'a');//value of 0 to 7
+
+                System.out.print("Valid moves from \"" + from + "\" to: ");
+
+                for (byte i = 0; i < 8; ++i)//loop for rows
                 {
-                    if(board.validMove(fromY, fromX, i, j))
-                        System.out.print(chessCoordinatesOf(i, j) + " ");
+                    for (byte j = 0; j < 8; ++j)//loop for coloums
+                    {
+                        if(board.validMove(fromY, fromX, i, j))
+                            System.out.print(chessCoordinatesOf(i, j) + " ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
+            catch (StringIndexOutOfBoundsException e)
+            {
+                System.out.println("Invalid syntax");
+                System.out.println("Type \"help\" for help");
+            }
         }
         else
         {
@@ -352,5 +399,54 @@ public class CLI//will eventually take over from main function with actual user 
     private static String chessCoordinatesOf(byte y, byte x)
     {
         return Character.toString((char) ('a' + x)) + (8 - y);
+    }
+    
+    private static void saveTheUserFromHeadaches()
+    {
+        
+        System.out.println("\nCommands");
+        
+        System.out.println("╔══════╦═══════════════════════════════════════════════════╗");
+        System.out.println("║print ║Print the chess board to the terminal again.       ║");
+        System.out.println("║move  ║Move a piece to another location. (eg. move a2 a3) ║");
+        System.out.println("║list  ║List the valid moves of a piece.                   ║");
+        System.out.println("║save  ║Save the board to disk.      (eg. save foobar.brd) ║");
+        System.out.println("║load  ║Load the board from disk.    (eg. load foobar.brd) ║");
+        System.out.println("║help  ║Display information about various cheja commands.  ║");
+        System.out.println("║about ║Display information about cheja itself cheja       ║");
+        System.out.println("║exit  ║Quit cheja (be sure to save first).                ║");
+        System.out.println("╚══════╩═══════════════════════════════════════════════════╝");
+    }
+    
+    private static void displayAbout()
+    {
+        System.out.println("\ncheja " + Cheja.VERSION);
+        System.out.println("Monolithic chess game for the terminal and the desktop!");
+        System.out.println("Built from the ground up by Rowan Massad and John Jekel\n");
+        
+        System.out.println("Licencing Information");
+        System.out.println("╔════════════════════════════════════════════════════════════════════════════════╗");
+        System.out.println("║MIT License                                                                     ║");
+        System.out.println("║                                                                                ║");
+        System.out.println("║Copyright (c) 2019 John Jekel                                                   ║");
+        System.out.println("║                                                                                ║");
+        System.out.println("║Permission is hereby granted, free of charge, to any person obtaining a copy    ║");
+        System.out.println("║of this software and associated documentation files (the \"Software\"), to deal   ║");//to componsate for escape characters
+        System.out.println("║in the Software without restriction, including without limitation the rights    ║");
+        System.out.println("║to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       ║");
+        System.out.println("║copies of the Software, and to permit persons to whom the Software is           ║");
+        System.out.println("║furnished to do so, subject to the following conditions:                        ║");
+        System.out.println("║                                                                                ║");
+        System.out.println("║The above copyright notice and this permission notice shall be included in all  ║");
+        System.out.println("║copies or substantial portions of the Software.                                 ║");
+        System.out.println("║                                                                                ║");
+        System.out.println("║THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      ║");//to componsate for escape characters
+        System.out.println("║IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        ║");
+        System.out.println("║FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     ║");
+        System.out.println("║AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          ║");
+        System.out.println("║LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   ║");
+        System.out.println("║OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   ║");
+        System.out.println("║SOFTWARE.                                                                       ║");
+        System.out.println("╚════════════════════════════════════════════════════════════════════════════════╝");
     }
 }
